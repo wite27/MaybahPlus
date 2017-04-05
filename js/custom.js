e8502c23af112916d798a713d28dac3854373c3b
wite27 <xquzyx@yandex.ru>
2017-04-06 01:49:42 +0300

    removed locations

--- js/custom.js
+++ js/custom.js
@@ -163,7 +163,7 @@ function loadMap(addressData){
 
 loadMap(locations[0].value);
 
-$("#location-map-select").append('<option value="'+locations[0].value+'">Please select a location</option>');  
+$("#location-map-select").append('<option value="'+locations[0].value+'">Пожалуйста, выберите офис</option>');  
 $.each(locations, function( index, value ) {
   var option = '<option value="'+value.value+'">'+value.value+'</option>';
   $("#location-map-select").append(option);
